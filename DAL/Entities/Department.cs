157ded827564f36dd83b3ec57514bcb89e32b821
Max Matvienko <maxmatvienko26@gmail.com>
2023-11-23 16:28:38 +0200

    Added Entities to DAL

---
+++ DAL/Entities/Department.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class Department
+    {
+        public int DepartmentID { get; set; }
+
+        public string Name { get; set; }
+
+        public List<Project> ListProjects { get; set; }
+    }
+}
---
+++ DAL/Entities/Project.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class Project
+    {
+        public int ProjectID { get; set; }
+
+        public string Name { get; set; }
+
+        public List<Department> ListDepartments { get; set; }
+    }
+}
