fb749b57403bf82176e1a04448e6e123556f3c6b
Max Matvienko <maxmatvienko26@gmail.com>
2023-11-23 17:57:33 +0200

    Added CLL project

---
+++ CCL/Security/Identity/Accountant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCL.Security.Identity
+{
+    public class Accountant : User
+    {
+        public Accountant(string firstName, string lastName, string departmentId) : base(firstName, lastName, departmentId, nameof(Accountant))
+        {
+        }
+    }
+}
